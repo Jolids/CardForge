555b1b881e2d1c423ec5f6015ed5d8bafd64ef4d
Jolids <jolids@gmail.com>
2026-09-20 01:42:24 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Supabase publishable/anon key предназначен для браузерных приложений.
 window.CARDFORGE_CONFIG = {
   API_BASE_URL: "https://31.76.56.162",
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: ""
+  SUPABASE_URL: "https://lbhhcciutyglukllhmjh.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_1SOfkB5t-_Xo-IvZZZy8oQ_RAU-ua6G"
 };
